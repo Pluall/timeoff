2297035d7476a46390d0216402ac8b7e96988a4a
Pluall <mmmferreira1@gmail.com>
2025-03-26 18:10:33 +0000

    Created Input Atom

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button } from '@/components/atoms/Button';
+import { Input } from '@/components/atoms/Input';
 import { Typography } from '@/components/atoms/Typography';
 import { redirect } from 'next/navigation';
 
@@ -8,34 +9,33 @@ const Page = () => {
     redirect('/');
   }
   return (
-    <div>
-      <div className={'flex flex-col items-start gap-5 p-4'}>
-        <div>
-          <Button variant={'default'}>Primary</Button>
-          <Button variant={'secondary'}>Secondary</Button>
-          <Button variant={'ghost'}>Ghost</Button>
-          <Button variant={'destructive'}>Destructive</Button>
-          <Button variant={'link'}>Link</Button>
-        </div>
-        <Typography as={'h1'} size={'5xl'}>
-          H1
-        </Typography>
-        <Typography as={'h2'} size={'4xxl'}>
-          H2
-        </Typography>
-        <Typography as={'h3'} size={'3xxl'}>
-          H3
-        </Typography>
-        <Typography as={'h4'} size={'2xl'}>
-          H4
-        </Typography>
-        <Typography as={'h5'} size={'lg'}>
-          H5
-        </Typography>
-        <Typography as={'h6'} size={'sm'}>
-          H6
-        </Typography>
+    <div className={'flex flex-col items-start gap-5 p-4'}>
+      <div>
+        <Button variant={'default'}>Primary</Button>
+        <Button variant={'secondary'}>Secondary</Button>
+        <Button variant={'ghost'}>Ghost</Button>
+        <Button variant={'destructive'}>Destructive</Button>
+        <Button variant={'link'}>Link</Button>
       </div>
+      <Typography as={'h1'} size={'5xl'}>
+        H1
+      </Typography>
+      <Typography as={'h2'} size={'4xxl'}>
+        H2
+      </Typography>
+      <Typography as={'h3'} size={'3xxl'}>
+        H3
+      </Typography>
+      <Typography as={'h4'} size={'2xl'}>
+        H4
+      </Typography>
+      <Typography as={'h5'} size={'lg'}>
+        H5
+      </Typography>
+      <Typography as={'h6'} size={'sm'}>
+        H6
+      </Typography>
+      <Input />
     </div>
   );
 };
